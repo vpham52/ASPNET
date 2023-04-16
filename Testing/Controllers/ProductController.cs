6c53ffe184c147a360a2ce738bc96e8a05dd10f0
Victoria Pham <victoriapham52@gmail.com>
2023-04-15 18:58:37 -0700

    ASP Product 3 commit

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Testing.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -23,7 +24,30 @@ namespace Testing.Controllers
             var products = repo.GetAllProducts();
             return View(products);
         }
-      
+
+        public IActionResult ViewProduct(int id)
+        {
+            var product = repo.GetProduct(id);
+            return View(product);
+        }
+
+        public IActionResult UpdateProduct(int id)
+        {
+            Product prod = repo.GetProduct(id);
+            if (prod == null)
+            {
+                return View("ProductNotFound");
+            }
+            return View(prod);
+        }
+
+        public IActionResult UpdateProductToDatabase(Product product)
+        {
+            repo.UpdateProduct(product);
+
+            return RedirectToAction("ViewProduct", new { id = product.ProductID });
+        }
+
     }
 }
 
--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -7,6 +7,8 @@ namespace Testing
 	public interface IProductRepository
 	{
         public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+        public void UpdateProduct(Product product);
 
     }
     
