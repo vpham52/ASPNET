6b483f1f53eeed938b1d64c9d603605950fae4f2
Victoria Pham <victoriapham52@gmail.com>
2023-04-15 18:32:15 -0700

    Intro commit

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Testing.Models;
+
+namespace Testing
+{
+	public interface IProductRepository
+	{
+        public IEnumerable<Product> GetAllProducts();
+
+    }
+    
+}
+
